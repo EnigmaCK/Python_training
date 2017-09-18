6288438c8bebcc522c9a9c178db3a5333af13af2
Nataliia_H <enigma.ck24@gmail.com>
2017-09-18 18:07:23 +0300

    Созданы фикстуры для тестов "test_add_group" и "test_add_contact"

--- test_add_contact.py
+++ test_add_contact.py
@@ -1,78 +1,24 @@
 # -*- coding: utf-8 -*-
-from selenium.webdriver.firefox.webdriver import WebDriver
-import unittest
+import pytest
 from contact import Contact
+from application_contact import ApplicationContact
 
 
-def is_alert_present(wd):
-    try:
-        wd.switch_to_alert().text
-        return True
-    except:
-        return False
+@pytest.fixture
+def app(request):
+    fixture = ApplicationContact()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
 
-class TestAddContact(unittest.TestCase):
-    def setUp(self):
-        self.wd = WebDriver(capabilities={"marionette": False})
-        self.wd.implicitly_wait(60)
-
-    def open_home_page(self, wd):
-        wd.get("http://localhost/addressbook/")
-
-    def login(self, wd, username, password):
-        self.open_home_page(wd)
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
-
-    def open_add_new_contact_page(self, wd):
-        wd.find_element_by_link_text("add new").click()
-
-    def create_new_contact(self, wd, contact):
-        self.open_add_new_contact_page(wd)
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contact.name)
-        wd.find_element_by_name("theform").click()
-        wd.find_element_by_name("mobile").click()
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(contact.mobile)
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(contact.email)
-        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
-        self.return_to_home_page(wd)
-
-    def return_to_home_page(self, wd):
-        wd.find_element_by_link_text("home page").click()
-
-    def logout(self, wd):
-        wd.find_element_by_link_text("Logout").click()
-    
-    def test_add_contact(self):
-        success = True
-        wd = self.wd
-        self.login(wd, username="admin", password="secret")
-        self.create_new_contact(wd, Contact(name="Natasha", mobile="55555555555", email="nataliia.hubenko@gmail.com"))
-        self.logout(wd)
-        self.assertTrue(success)
-
-    def test_add_second_contact(self):
-        success = True
-        wd = self.wd
-        self.login(wd, username="admin", password="secret")
-        self.create_new_contact(wd, Contact(name="Max", mobile="454545454545", email="max@gmail.com"))
-        self.logout(wd)
-        self.assertTrue(success)
-
-    def tearDown(self):
-        self.wd.quit()
+def test_add_contact(app):
+        app.login(username="admin", password="secret")
+        app.create_new_contact(Contact(name="Natasha", mobile="55555555555", email="nataliia.hubenko@gmail.com"))
+        app.logout()
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_add_second_contact(app):
+        app.login(username="admin", password="secret")
+        app.create_new_contact(Contact(name="Max", mobile="454545454545", email="max@gmail.com"))
+        app.logout()
+
