d7c5f3c091d13d4b3f6699417fc231be541d13bb
Nataliia_H <enigma.ck24@gmail.com>
2017-09-20 17:13:04 +0300

    Выделен класс для работы с контактами

--- fixture/application_contact.py
+++ fixture/application_contact.py
@@ -7,33 +7,10 @@ class ApplicationContact:
         self.wd = WebDriver(capabilities={"marionette": False})
         self.wd.implicitly_wait(60)
 
-    def open_home_page(self):
-        wd = self.wd
-        wd.get("http://localhost/addressbook/")
 
-    def open_add_new_contact_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("add new").click()
 
-    def create_new_contact(self, contact):
-        wd = self.wd
-        self.open_add_new_contact_page()
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contact.name)
-        wd.find_element_by_name("theform").click()
-        wd.find_element_by_name("mobile").click()
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(contact.mobile)
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(contact.email)
-        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
-        self.return_to_home_page()
 
-    def return_to_home_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("home page").click()
+
 
     def destroy(self):
         self.wd.quit()
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,25 +1,25 @@
 # -*- coding: utf-8 -*-
 import pytest
 
-from fixture.application_contact import ApplicationContact
+from fixture.application_group import ApplicationGroup
 from model.contact import Contact
 
 
 @pytest.fixture
 def app(request):
-    fixture = ApplicationContact()
+    fixture = ApplicationGroup()
     request.addfinalizer(fixture.destroy)
     return fixture
 
 
 def test_add_contact(app):
         app.session.login(username="admin", password="secret")
-        app.create_new_contact(Contact(name="Natasha", mobile="55555555555", email="nataliia.hubenko@gmail.com"))
+        app.contact.create(Contact(name="Natasha", mobile="55555555555", email="nataliia.hubenko@gmail.com"))
         app.session.logout()
 
 
 def test_add_second_contact(app):
         app.session.login(username="admin", password="secret")
-        app.create_new_contact(Contact(name="Max", mobile="454545454545", email="max@gmail.com"))
+        app.contact.create(Contact(name="Max", mobile="454545454545", email="max@gmail.com"))
         app.session.logout()
 
