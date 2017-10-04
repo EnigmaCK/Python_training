ee19805cce579fc0380410ae25f5f43c7cbc3c1c
Nataliia_H <enigma.ck24@gmail.com>
2017-10-04 21:14:13 +0300

    Улучшена проверка списка контактов в тесте на удаление контакта

--- model/contact.py
+++ model/contact.py
@@ -6,3 +6,9 @@ class Contact:
         self.mobile = mobile
         self.email = email
         self.id = id
+
+    def __repr__(self):
+        return "%s:%s" % (self.id, self.name)
+
+    def __eq__(self, other):
+        return self.id == other.id and self.name == other.name
