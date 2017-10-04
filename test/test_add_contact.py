141667888bb4af780e21fb13f1eab04c4959d80e
Nataliia_H <enigma.ck24@gmail.com>
2017-10-04 22:37:12 +0300

    Добавлены проверки, которые включают в себя сортировку списка контактов, в тесты для создания и модификации контактов

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -4,13 +4,19 @@ from model.contact import Contact
 
 def test_add_contact(app):
         old_contact = app.contact.get_contact_list()
-        app.contact.create(Contact(name="Natasha", mobile="55555555555", email="nataliia.hubenko@gmail.com"))
+        contact = Contact(name="Natasha", mobile="55555555555", email="nataliia.hubenko@gmail.com")
+        app.contact.create(contact)
         new_contact = app.contact.get_contact_list()
         assert len(old_contact) +1 == len(new_contact)
+        old_contact.append(contact)
+        assert sorted(old_contact,  key=Contact.id_or_max) == sorted(old_contact, key=Contact.id_or_max)
 
 
 def test_add_second_contact(app):
         old_contact = app.contact.get_contact_list()
-        app.contact.create(Contact(name="Max", mobile="454545454545", email="max@gmail.com"))
+        contact = Contact(name="Max", mobile="454545454545", email="max@gmail.com")
+        app.contact.create(contact)
         new_contact = app.contact.get_contact_list()
         assert len(old_contact) + 1 == len(new_contact)
+        old_contact.append(contact)
+        assert sorted(old_contact, key=Contact.id_or_max) == sorted(old_contact, key=Contact.id_or_max)
--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -6,15 +6,19 @@ def test_modify_contact_name(app):
     if app.contact.count() == 0:
         app.contact.create(Contact(name="New contact"))
     old_contact = app.contact.get_contact_list()
-    app.contact.modify_contact(Contact(name="Tasha"))
+    contact = Contact(name="Tasha")
+    contact.id = old_contact[0].id
+    app.contact.modify_contact(contact)
     new_contact = app.contact.get_contact_list()
     assert len(old_contact) == len(new_contact)
+    old_contact[0] = contact
+    assert sorted(old_contact, key=Contact.id_or_max) == sorted(new_contact, key=Contact.id_or_max)
 
 
-def test_modify_contact_mobile(app):
-    if app.contact.count() == 0:
-        app.contact.create(Contact(name="New contact"))
-    old_contact = app.contact.get_contact_list()
-    app.contact.modify_contact(Contact(mobile="888888888"))
-    new_contact = app.contact.get_contact_list()
-    assert len(old_contact) == len(new_contact)
+# def test_modify_contact_mobile(app):
+#     if app.contact.count() == 0:
+#         app.contact.create(Contact(name="New contact"))
+#     old_contact = app.contact.get_contact_list()
+#     app.contact.modify_contact(Contact(mobile="888888888"))
+#     new_contact = app.contact.get_contact_list()
+#     assert len(old_contact) == len(new_contact)
