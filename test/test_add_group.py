6daca5dda5f6067d1fbc17e55f343ad007e4842b
Nataliia_H <enigma.ck24@gmail.com>
2017-10-04 18:06:27 +0300

    Добавлены проверки для списка групп в тестах для создания, удаления и модификации групп

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -3,8 +3,15 @@ from model.group import Group
 
 
 def test_add_group(app):
+        old_groups = app.group.get_group_list()
         app.group.create(Group(name="test", header="test"))
+        new_groups = app.group.get_group_list()
+        assert len(old_groups) + 1 == len(new_groups)
 
 
 def test_add_empty_group(app):
+        old_groups = app.group.get_group_list()
         app.group.create(Group(name="", header=""))
+        new_groups = app.group.get_group_list()
+        assert len(old_groups) + 1 == len(new_groups)
+
