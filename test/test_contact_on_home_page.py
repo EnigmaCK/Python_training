f5f2572e6f38e804b3679b6ef6f186639a148c40
Nataliia_H <enigma.ck24@gmail.com>
2017-10-19 18:29:15 +0300

    Удалена функция clear для email на home page

--- test/test_contact_on_home_page.py
+++ test/test_contact_on_home_page.py
@@ -9,7 +9,7 @@ def test_contact_on_home_page(app):
     contact_from_home_page = app.contact.get_contact_list()[0]
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
     assert contact_from_home_page.all_phones_from_home_page == merge_contact_data_like_on_home_page(contact_from_edit_page, "phone")
-    assert contact_from_home_page.all_emails_from_home_page == merge_contact_data_like_on_home_page(contact_from_edit_page, "email")
+    assert clear(contact_from_home_page.all_emails_from_home_page) == merge_contact_data_like_on_home_page(contact_from_edit_page, "email")
     assert contact_from_home_page.address == contact_from_edit_page.address
     assert contact_from_home_page.name == contact_from_edit_page.name
     assert contact_from_home_page.lastname == contact_from_edit_page.lastname
@@ -37,8 +37,6 @@ def merge_contact_data_like_on_home_page(contact, param):
                                             contact.secondaryphone]))))
     elif param == "email":
         return "\n".join(filter(lambda x: x != "",
-                                map(lambda x: clear(x),
-                                    filter(lambda x: x is not None,
-                                           [contact.email, contact.secondemail, contact.thirdemail]))))
+                                [contact.email, contact.secondemail, contact.thirdemail]))
     else:
         print("Wrong parameter")
