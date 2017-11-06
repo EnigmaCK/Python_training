b0fa14bc92b38b8ce7ef9b1deb3b5588ffaa885e
Nataliia_H <enigma.ck24@gmail.com>
2017-11-06 18:57:35 +0200

    Добавлена новая фикстура для базы данных. В тестах на изменение, удаление и добавление групп добавлена выгрузка списка групп из базы данных. Так же в данные тесты добавлены отключаемые проверки пользовательского интерфейса

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -1,20 +1,20 @@
 
 from model.group import Group
-from random import randrange
+import random
 
 
-def test_modify_group_name(app):
-    if app.group.count() == 0:
+def test_modify_group_name(app, db, check_ui):
+    if len(db.get_group_list()) == 0:
         app.group.create(Group(name="test group"))
-    old_groups = app.group.get_group_list()
-    index = randrange(len(old_groups))
+    old_groups = db.get_group_list()
+    select_group = random.choice(old_groups)
     group = Group(name="test2")
-    group.id = old_groups[index].id
-    app.group.modify_group_by_index(index, group)
-    new_groups = app.group.get_group_list()
-    assert len(old_groups) == len(new_groups)
-    old_groups[index] = group
+    app.group.modify_group_by_id(select_group.id, group)
+    new_groups = db.get_group_list()
+    old_groups[old_groups.index(select_group)].name = group.name
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+    if check_ui:
+        assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
 
 
 # def test_modify_group_header(app):
