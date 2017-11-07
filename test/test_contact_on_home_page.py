c804ee0fe847bde12da330bafd419c96b2c67ad5
Nataliia_H <enigma.ck24@gmail.com>
2017-11-07 15:29:58 +0200

    Добавлен тест на сравнение списка контактов на главной странице и в базе данных

--- test/test_contact_on_home_page.py
+++ test/test_contact_on_home_page.py
@@ -11,17 +11,8 @@ def test_contact_on_home_page(app):
     assert contact_from_home_page.all_phones_from_home_page == merge_contact_data_like_on_home_page(contact_from_edit_page, "phone")
     assert clear(contact_from_home_page.all_emails_from_home_page) == merge_contact_data_like_on_home_page(contact_from_edit_page, "email")
     assert contact_from_home_page.address == contact_from_edit_page.address
-    assert contact_from_home_page.name == contact_from_edit_page.name
+    assert contact_from_home_page.firstname == contact_from_edit_page.firstname
     assert contact_from_home_page.lastname == contact_from_edit_page.lastname
-
-
-# def test_phones_on_contact_view_page(app):
-#     contact_from_view_page = app.contact.get_contact_from_view_page(0)
-#     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
-#     assert contact_from_view_page.homephone == contact_from_edit_page.homephone
-#     assert contact_from_view_page.workphone == contact_from_edit_page.workphone
-#     assert contact_from_view_page.mobile == contact_from_edit_page.mobile
-#     assert contact_from_view_page.secondaryphone == contact_from_edit_page.secondaryphone
 
 
 def clear(s):
