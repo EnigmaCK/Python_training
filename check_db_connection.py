5eeaef0f3747b716b480a11e37f07d522d51b0fc
Nataliia_H <enigma.ck24@gmail.com>
2017-12-01 12:10:01 +0200

    Созданы тесты на добавление и удаление группы с помощью Robot Framework

--- check_db_connection.py
+++ check_db_connection.py
@@ -1,7 +1,7 @@
 from fixture.orm import ORMFixture
 from model.group import Group
 
-db = ORMFixture(host='127.0.0.1', name="addressbook", user="root", password="")
+db = ORMFixture(host='127.0.0.1', name="addressbook", user="root", password="")#db.destroy()
 
 try:
     l = db.get_contacts_in_group(Group(id="2"))
@@ -9,4 +9,4 @@ try:
         print(item)
     print(len(l))
 finally:
-    pass #db.destroy()
+    pass
